3cb5bab74167928582dda33eb1b6a52708167f8c
madycakes <viclee02@gmail.com>
2018-06-11 16:29:40 -1000

    revamp project

--- js/app.js
+++ js/app.js
@@ -1,30 +1,40 @@
 /*Hey Prepsters! Let's help Ronald McDonald find true love by revising his online dating profile!*/
 
-// 1. In the nav element, target the span element with the id of "count" and change the number from 0 to 8.
+//1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
+
+//2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
+
+//3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
+
+//4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
+
+//5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
+
+//6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
+
+//7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
+
+//8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
+
+//9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
+
+//10. In the div id matches section, target the first div element with the class name of firstName and change Name to Wendy.
+
+//11. In the div id matches section, target the first div element with the class name of otherAge and change Age to 48.
+
+//12. In the div id matches section, target the first div element with the class name of status and change Status to Single Mother.
+
+//13. In the div id matches section, target the second div element with the class name of firstName and change Name to Peko Chan.
+
+//14. In the div id matches section, target the second div element with the class name of otherAge and change Age to 68.
+
+ //Final Boss Create your own profile into the page:
+ //change image to match your profile
+ //div with class name of firstName
+ //div with class name of otherAge
+ //div with class name of status
+ //div with class anem of Motto
 
 
 
-// 2. Target the div with the id of 'age' and change the contents to: "21 year old vibrant stud".
-
-
-
-// 3. Target the span element with the id of "distance" and change the number from 1000 to 168.
-
-
-// 4. Create a paragraph element with the id of "pro2". In this paragraph, add the following content: "My main objective is to find a Mrs. McDonald." Append this element inside of the paragraph element with the id of "profile".
-
-
-// 5. Create a span element of the id of "mail" and give it a content of 11. Append this element into the "Messages" li element.
-
-
-//6. Target the div id "first-name1" and replace the text "First Name" to "Wendy".
-
-
-//7. Target the div id "first-name2" and replace the text "First Name" to "Marie".
-
-
-//8. Target the div id "first-name3" and replace the text "First Name" to "Anna".
-
-
-//9. Target the div classes "last-name" and replace the text "Last Name" for each person. 
 
