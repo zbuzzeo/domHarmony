26cf69c59084f4058d5d48121d311bf908546841
unknown <zeke.buzzeo@gmail.com>
2018-10-18 09:59:15 -1000

    Exericse 6

--- js/app.js
+++ js/app.js
@@ -46,6 +46,15 @@ console.log(newDiv);
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
+var divHobbies = document.createElement('div');
+
+divHobbies.id = 'hobbies';
+divHobbies.innerHTML = 'Long romantic walks on the beach, candle-lit Big Mac dinners and tormenting the King.';
+data.appendChild(divHobbies);
+
+spacer(6);
+console.log(divHobbes);
+
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
