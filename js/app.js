8725171783506bb275211af856fb9f0e02e161a3
unknown <zeke.buzzeo@gmail.com>
2018-10-18 10:02:19 -1000

    Exericse 8

--- js/app.js
+++ js/app.js
@@ -68,6 +68,15 @@ console.log(divLocation);
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
 
+var divWants = document.createElement('div');
+
+divWants.id = 'wants';
+divWants.innerHTML = 'Looking for a Mrs. McDonald.';
+data.appendChild(divWants);
+
+spacer(8);
+console.log(divWants);
+
 //9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
 
 //10. In the div id matches section, target the first div element with the class name of firstName and change Name to Wendy.
