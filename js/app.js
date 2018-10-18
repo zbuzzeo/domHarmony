ecce008def3c58d4c695a7d02f774727175be04d
unknown <zeke.buzzeo@gmail.com>
2018-10-18 09:57:31 -1000

    Exercise 5

--- js/app.js
+++ js/app.js
@@ -6,33 +6,43 @@ function spacer(exerciseNum){
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
 
-spacer(1);
 matCount.innerHTML = 11;
 
+spacer(1);
 console.log(matCount);
 
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
 
-spacer(2);
 msgCount.innerHTML = 23;
 
+spacer(2);
 console.log(msgCount);
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
-spacer(3);
 fullname.innerHTML = 'Ronald McDonald';
 
+spacer(3);
 console.log(fullname);
 
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
 
-spacer(4);
 age.innerHTML = '63 Years Old'
 
+spacer(4);
 console.log(age);
 
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
+
+var newDiv = document.createElement('div');
+var data = document.getElementById('data');
+
+newDiv.id = 'job';
+newDiv.innerHTML = 'Clown and Restauranteur';
+data.appendChild(newDiv);
+
+spacer(5);
+console.log(newDiv);
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
